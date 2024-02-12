ac38ad547106aed8b34236785b7e83be25bc6146
Katja <katja@klahr.se>
2024-02-12 14:41:18 +0100

    Delete button implemented

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var task = ""
+    @State var todoItems = [
+        TodoItem(title: "Somna", isCompleted: true),
+        TodoItem(title: "Vakna", isCompleted: true)
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Katjas todo-app")
+            List {
+                ForEach(todoItems) { item in
+                    TodoItemView(todoItem: item, todoItems: $todoItems)
+                }
+            }
+            .padding()
+            TextField("Ny uppgift", text: $task)
+                .padding()
+            Button("Lägg till uppgift") {
+                buttonFunction()
+            }
+            .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .cornerRadius(20)
+        
         }
         .padding()
     }
+    
+    func buttonFunction() {
+        todoItems.append(TodoItem(title: task, isCompleted: false))
+        task = ""
+    }
 }
+
 
 #Preview {
     ContentView()
