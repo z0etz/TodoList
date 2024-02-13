7c6624384938328ed0a149f671afc9984b3a6389
Katja <katja@klahr.se>
2024-02-13 17:29:06 +0100

    MVVM with core data version finished.

---
+++ TodoList/TaskItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskItemView.swift
+//  TodoList
+//
+//  Created by Katja Klahr on 2024-02-13.
+//
+
+import SwiftUI
+
+struct TaskItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskItemView()
+}
